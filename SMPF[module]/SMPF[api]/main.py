7d23e75bdbce57e1d0cbf745e33ea7752b1f7689
John Markton Olarte <jmolarte@up.edu.ph>
2022-10-08 22:03:44 +0800

    Updated SMPF[api]
    Test endpoints completed

--- SMPF[module]/SMPF[api]/main.py
+++ SMPF[module]/SMPF[api]/main.py
@@ -1,8 +1,10 @@
+from multiprocessing import AuthenticationError
 from matplotlib import collections
 from fastapi import FastAPI
 # import models from ../SMPF[database]/models.py
 from database.models import Buy
 from mongoengine import connect
+import json
 
 app = FastAPI()
 connect(db="smpf", host="localhost", port=27017)
@@ -11,10 +13,38 @@ connect(db="smpf", host="localhost", port=27017)
 def home():
     return {"message": "Welcome to Stock Market Price Forecasting API developed by John Markton M. Olarte"}
 
-# Test endpoint
-@app.get("/test")
+# buy
+# NOTE: TEST ONLY
+@app.get("/buy")
 def test():
     # Get all data from the "Buy" collection
-    data = Buy.objects()
+    data = Buy.objects().to_json()
+    json_data = json.loads(data)
     # Return the data
-    return data.to_json()
+    return {"Test Data": json_data}
+
+# Purge all data from the "Buy" collection
+# NOTE: TEST ONLY
+@app.delete("/purge_buy")
+def purge_buy():
+    Buy.objects().delete()
+    return {"message": "All data from the 'Buy' collection has been purged."}
+
+# Put data from test.json to the "Buy" collection
+# NOTE: TEST ONLY
+@app.post("/put_buy")
+def put_buy():
+    # Open test.json
+    with open("./database/json_data/test.json", "r") as f:
+        # Read the file
+        data = f.read()
+        # Load the data
+        json_data = json.loads(data)
+        # Put the data to the "Buy" collection
+        for item in json_data:
+            buy = Buy(**item)
+            buy.save()
+    f.close()
+        
+    # Return the data
+    return {"message": "Data from 'test.json' has been put to the 'Buy' collection."}
