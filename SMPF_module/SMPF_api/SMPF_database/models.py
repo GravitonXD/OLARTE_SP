b257e4bd26836d00f8c79ad99af345b84b1e2404
John Markton Olarte <jmolarte@up.edu.ph>
2022-10-09 14:21:55 +0800

    Added models for Sell

--- SMPF_module/SMPF_api/SMPF_database/models.py
+++ SMPF_module/SMPF_api/SMPF_database/models.py
@@ -18,3 +18,20 @@ class Buy(Document):
             "stock_symbol": self.stock_symbol,
             "last_closing": self.last_closing
         }
+    
+# Define class "Sell" and inherit from "Document"
+class Sell(Document):
+    stock_symbol =  StringField()
+    last_closing =  FloatField()
+
+    # When putting data to the "Sell" collection, the data will be in this format
+    def from_json(self, json_data):
+        self.stock_symbol = json_data.get("stock_symbol")
+        self.last_closing = json_data.get("last_closing")
+
+    # Format the data to JSON
+    def to_json(self):
+        return {
+            "stock_symbol": self.stock_symbol,
+            "last_closing": self.last_closing
+        }
