a83e673bae988763218e6810658102c72e7a073a
John Markton Olarte <jmolarte@up.edu.ph>
2022-10-13 21:59:07 +0800

    Initialized ml_processor

---
+++ src/preprocessor/data_processor/ml_processor.py
@@ -0,0 +1,20 @@
+import pickle
+
+def load_model(model_path):
+    pass
+
+def load_data(data_path):
+    pass
+
+def process_data(data):
+    pass
+
+def save_to_json(data, path):
+    pass
+
+def main():
+    pass
+    # TODO: load the model from the pickle file
+    # TODO: load the data from the csv file
+    # TODO: process the data
+    # TODO: save the data to the json files and put them in the json_data folder
