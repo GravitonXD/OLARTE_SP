e884c01c85cc601d9916cc26e9963119ec1053f5
John Markton Olarte <jmolarte@up.edu.ph>
2023-01-14 21:56:08 +0800

    sample testing (STATS LOGGING)

--- testers/stats_logging/stats_log.sh
+++ testers/stats_logging/stats_log.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 echo "Logging has started."
+# current time
+echo $(date)
 python3 stats_logging.py
 echo "Logging has ended."
+echo $(date)
 echo "Processing has started."
 python3 stats_log_processing.py
 echo "Processing has ended."
+echo $(date)
--- testers/stats_logging/stats_logging.py
+++ testers/stats_logging/stats_logging.py
@@ -4,9 +4,9 @@ from time import sleep
 # Run docker stats command
 timer = 0
 # Total log time expected in seconds
-#   note: 1 day = 86400 seconds
-total_log_time = 60
+#   note: 1 day = 86400 seconds (but the loop runs every 3 seconds)
+#   hence, 86400/3 = 28800
+total_log_time = 60 // 3
 while timer < total_log_time:
     os.system("docker stats --no-stream >> stats.txt")
-    sleep(1)
     timer += 1
