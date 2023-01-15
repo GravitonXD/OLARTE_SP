9733f908777744a2f9ecd8e93130e89e7675261e
John Markton Olarte <jmolarte@up.edu.ph>
2023-01-15 19:21:27 +0800

    Updated Stats_Logging

--- testers/stats_logging/stats_logging.py
+++ testers/stats_logging/stats_logging.py
@@ -6,7 +6,7 @@ timer = 0
 # Total log time expected in seconds
 #   note: 1 day = 86400 seconds (but the loop runs every 3 seconds)
 #   hence, 86400/3 = 28800
-total_log_time = 60 // 3
+total_log_time = (60 * 60 * 3) // 3
 while timer < total_log_time:
     os.system("docker stats --no-stream >> stats.txt")
     timer += 1
