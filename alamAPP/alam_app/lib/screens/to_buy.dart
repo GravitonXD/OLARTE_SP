dffcd9124094a3583af64f56313daea854a2d05b
GravitonXD <jmolarte@up.edu.ph>
2023-02-06 00:11:45 +0800

    Initialized Screens and Navigation Bar

---
+++ alamAPP/alam_app/lib/screens/to_buy.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class BuyScreen extends StatefulWidget {
+  @override
+  _BuyScreenState createState() => _BuyScreenState();
+}
+
+class _BuyScreenState extends State<BuyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: const Center(
+        child: Text('Buy Screen'),
+      ),
+    );
+  }
+}
---
+++ alamAPP/alam_app/lib/screens/to_sell.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SellScreen extends StatefulWidget {
+  @override
+  _SellScreenState createState() => _SellScreenState();
+}
+
+class _SellScreenState extends State<SellScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: const Center(
+        child: Text('Sell Screen'),
+      ),
+    );
+  }
+}
