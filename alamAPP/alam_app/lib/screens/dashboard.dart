32944af0a9325430ae062b1e683c40641495c697
GravitonXD <jmolarte@up.edu.ph>
2023-02-06 22:10:59 +0800

    Updated "Buy" and "Sell" Screens

--- alamAPP/alam_app/lib/screens/dashboard.dart
+++ alamAPP/alam_app/lib/screens/dashboard.dart
@@ -11,100 +11,98 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.fromLTRB(25, 50, 25, 25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // Welcome Text
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Circular Avatar
-                  CircleAvatar(
-                    radius: 45,
-                    backgroundColor: Colors.grey[300],
-                    child: ClipOval(
-                      child: Image.network(
-                        "https://img.freepik.com/premium-vector/stylish-flat-black-white-human-avatar-social-media-presentation-people-avatar-icon-avatar-face-head-with-forearm-human-portrait-isolated-blue-background-vector-graphics_589396-126.jpg?w=2000",
-                      ),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(25, 50, 25, 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // Welcome Text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Circular Avatar
+                CircleAvatar(
+                  radius: 45,
+                  backgroundColor: Colors.grey[300],
+                  child: ClipOval(
+                    child: Image.network(
+                      "https://img.freepik.com/premium-vector/stylish-flat-black-white-human-avatar-social-media-presentation-people-avatar-icon-avatar-face-head-with-forearm-human-portrait-isolated-blue-background-vector-graphics_589396-126.jpg?w=2000",
                     ),
                   ),
-                  // Welcome
-                  Text(
-                    "Welcome Back, \nMarkton",
-                    style: StandardFontStyle.titleBlack,
-                  )
-                ],
-              ),
-              // Portfolio Bar and Add Button
-              const Divider(
-                color: Colors.white,
-                height: 30,
-              ),
-              Row(
-                // Portfolio Bar
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Transform.translate(
-                    offset: const Offset(-25, 0),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                        topRight: Radius.circular(20),
-                        bottomRight: Radius.circular(20),
-                      ),
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: 45,
-                        child: ColoredBox(
-                          color: const Color.fromARGB(255, 66, 165, 245),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              const Icon(
-                                Icons.work,
-                                color: Colors.white,
-                                size: 25,
-                              ),
-                              Text(
-                                "Portfolio",
-                                style: StandardFontStyle.headingWhite,
-                              ),
-                            ],
-                          ),
+                ),
+                // Welcome
+                Text(
+                  "Welcome Back, \nMarkton",
+                  style: StandardFontStyle.titleBlack,
+                )
+              ],
+            ),
+            // Portfolio Bar and Add Button
+            const Divider(
+              color: Colors.white,
+              height: 30,
+            ),
+            Row(
+              // Portfolio Bar
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Transform.translate(
+                  offset: const Offset(-25, 0),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: 45,
+                      child: ColoredBox(
+                        color: const Color.fromARGB(255, 66, 165, 245),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            const Icon(
+                              Icons.work,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                            Text(
+                              "Portfolio",
+                              style: StandardFontStyle.headingWhite,
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ),
-                  // Add Button
+                ),
+                // Add Button
 
-                  GestureDetector(
-                    onTap: () => print("Add Button Pressed"),
-                    child: const ClipRRect(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                      child: SizedBox(
-                        width: 55,
-                        height: 45,
-                        child: ColoredBox(
-                          color: Color.fromARGB(255, 129, 199, 132),
-                          child: Icon(
-                            Icons.add,
-                            color: Colors.white,
-                            size: 25,
-                          ),
+                GestureDetector(
+                  onTap: () => print("Add Button Pressed"),
+                  child: const ClipRRect(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    child: SizedBox(
+                      width: 55,
+                      height: 45,
+                      child: ColoredBox(
+                        color: Color.fromARGB(255, 129, 199, 132),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                          size: 25,
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
-              // Portfolio List Tiles
-              Row(),
-            ],
-          ),
+                ),
+              ],
+            ),
+            // Portfolio List Tiles
+            Row(),
+          ],
         ),
       ),
     );
--- alamAPP/alam_app/lib/screens/to_sell.dart
+++ alamAPP/alam_app/lib/screens/to_sell.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:alam_app/standards/font_styles.dart';
 
 class SellScreen extends StatefulWidget {
   @override
@@ -6,12 +7,87 @@ class SellScreen extends StatefulWidget {
 }
 
 class _SellScreenState extends State<SellScreen> {
+  // Search Bar Controller
+  final searchController = TextEditingController();
+  String searchText = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: const Center(
-        child: Text('Sell Screen'),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(25, 50, 25, 25),
+        child: Row(
+          // To Buy Title
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Transform.translate(
+              offset: const Offset(-25, 0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  height: 45,
+                  child: ColoredBox(
+                    color: const Color.fromARGB(255, 66, 165, 245),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Stocks to Sell",
+                          style: StandardFontStyle.headingWhite,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // Search Bar
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: TextField(
+                controller: searchController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Search Stock',
+                  prefixIcon: const Icon(Icons.search_outlined),
+                  suffixIcon: searchController.text.isEmpty
+                      ? null
+                      : IconButton(
+                          icon: const Icon(Icons.clear),
+                          onPressed: () {
+                            setState(
+                              () {
+                                // Clear input text field
+                                searchController.clear();
+                                searchText = "";
+                              },
+                            );
+                          },
+                        ),
+                  labelStyle: StandardFontStyle.bodyBlack,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onSubmitted: (value) {
+                  setState(
+                    () {
+                      // Search Bar
+                      searchText = value;
+                      print(searchText);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
