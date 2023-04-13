1fe1b5cbbc5532d2258bd32ca13d3c100a9bc1c6
GravitonXD <jmolarte@up.edu.ph>
2023-04-13 20:50:46 +0800

    Update 0-stats_logging.py

--- testers/1-stats_logging/0-stats_logging.py
+++ testers/1-stats_logging/0-stats_logging.py
@@ -3,10 +3,10 @@ import time
 import os
 
 def job():
-    print("Logging Performance Data \nInterrupt with Ctrl+C")
+    print("Logging Idle Performance Data \nInterrupt with Ctrl+C")
     while True:
         # Log the performance data
-        os.system("docker stats --no-stream >> ./load_performance/stats.txt")
+        os.system("docker stats --no-stream >> ./raw_data/stats.txt")
 
 def stop_job():
     try:
@@ -17,11 +17,11 @@ def stop_job():
 
 def main():
     schedule.every().day.at("21:00").do(job)
-    schedule.every().day.at("22:00").do(stop_job)
+    schedule.every().day.at("10:31").do(stop_job)
 
     while True:
         schedule.run_pending()
-        os.makedirs('./load_performance', exist_ok=True)
+        os.makedirs('./raw_data', exist_ok=True)
         # Print the remaining time until the next scheduled job
         print(f'Logger will run in {schedule.idle_seconds()} seconds', end='\r')
         time.sleep(1)
