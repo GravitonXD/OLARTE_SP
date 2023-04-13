53641cc15b90fff679e05bd1d1d94651c588258f
GravitonXD <jmolarte@up.edu.ph>
2023-04-13 20:50:04 +0800

    Update 0-stats_logging.py

--- testers/1-stats_logging/0-stats_logging.py
+++ testers/1-stats_logging/0-stats_logging.py
@@ -1,16 +1,30 @@
+import schedule
+import time
 import os
 
-# Run docker stats command
-timer = 0
-# Total log time expected in seconds
-#   note: 1 day = 86400 seconds (but the loop runs every 3 seconds)
-#   hence, 86400/3 = 28800
-seconds, minutes, hours = 60, 60, 24
-total_log_time = 10
-os.system("date > ./raw_data/start_time.txt")
-print(f'Logging for {hours} hours')
-while timer < total_log_time:
-    os.system("docker stats --no-stream >> ./raw_data/stats.txt")
-    timer += 1
-os.system("date > ./raw_data/end_time.txt")
-print('Logging complete')
+def job():
+    print("Logging Performance Data \nInterrupt with Ctrl+C")
+    while True:
+        # Log the performance data
+        os.system("docker stats --no-stream >> ./load_performance/stats.txt")
+
+def stop_job():
+    try:
+        schedule.clear()
+        exit(0)
+    except:
+        exit(1)
+
+def main():
+    schedule.every().day.at("21:00").do(job)
+    schedule.every().day.at("22:00").do(stop_job)
+
+    while True:
+        schedule.run_pending()
+        os.makedirs('./load_performance', exist_ok=True)
+        # Print the remaining time until the next scheduled job
+        print(f'Logger will run in {schedule.idle_seconds()} seconds', end='\r')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
